21add95d481b7b865518dc694e3c664e1a1231ea
Art_Line <serg20004@ukr.net>
2023-03-07 01:45:17 +0100

    filter categories from array, add active filter

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 function Filters() {
+    const categories = ['All burgers', 'Simple burgers', 'Double burgers', 'Veggie burger', 'Spicy burgers'];
+    const [currentCategory, setCurrentCategory] = useState(0);
     return (
         <ul className="filters">
-            <li><button className="filters__btn active" type="button">All burgers</button></li>
-            <li><button className="filters__btn" type="button">Simple burgers</button></li>
-            <li><button className="filters__btn" type="button">Double burgers</button></li>
-            <li><button className="filters__btn" type="button">Veggie burgers</button></li>
-            <li><button className="filters__btn" type="button">Spicy burgers</button></li>
+            {categories.map((elem, index) => {
+                return (
+                    <li key={index}>
+                        <button
+                            onClick={() => setCurrentCategory(index)}
+                            className={`filters__btn ${currentCategory === index ? 'active' : ''}`}
+                            type="button"
+                        >
+                            {elem}
+                        </button>
+                    </li>
+                )
+            })}
         </ul>
     )
 }
