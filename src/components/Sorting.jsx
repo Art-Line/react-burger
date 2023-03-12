dbc8624220e7ebcb870b9b8c5cb54e2df62baf0e
Art_Line <serg20004@ukr.net>
2023-03-13 00:04:38 +0100

    sorting tooltip and select sorting type

--- src/components/Sorting.jsx
+++ src/components/Sorting.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 function Sorting() {
+    const [sortTooltip, setSortTooltip] = useState(false);
+    const sortList = ['popularity A-Z', 'popularity Z-A', 'price A-Z', 'price Z-A', 'alphabet A-Z', 'alphabet Z-A'];
+    const [sortActive, setSortActive] = useState(0);
+
+    const selectSort = (index) => {
+        setSortActive(index);
+        setSortTooltip(false);
+    }
+
     return (
         <div className="sort">
-            <button className="sort__btn" type="button">Sort by: popularity A-Z</button>
-            <ul className="sort__tooltip" style={{ 'display': 'none' }}>
-                <li><button className="sort__tooltip-btn active" type="button">popularity A-Z</button></li>
-                <li><button className="sort__tooltip-btn" type="button">popularity Z-A</button></li>
-                <li><button className="sort__tooltip-btn" type="button">price A-Z</button></li>
-                <li><button className="sort__tooltip-btn" type="button">price Z-A</button></li>
-                <li><button className="sort__tooltip-btn" type="button">alphabet A-Z</button></li>
-                <li><button className="sort__tooltip-btn" type="button">alphabet Z-A</button></li>
-            </ul>
+            <button
+                type="button"
+                className={`sort__btn ${sortTooltip ? 'active' : ''}`}
+                onClick={() => setSortTooltip(!sortTooltip)}
+            >
+                Sort by: {sortList[sortActive]}
+            </button>
+            {sortTooltip &&
+                <ul className="sort__tooltip">
+                    {sortList.map((item, index) =>
+                        <li key={index}>
+                            <button
+                                type="button"
+                                onClick={() => selectSort(index)}
+                                className={`sort__tooltip-btn ${sortActive === index ? 'active' : ''}`}
+                            >
+                                {item}
+                            </button>
+                        </li>
+                    )}
+                </ul>
+            }
         </div>
     )
 }
